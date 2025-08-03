2fe6b009b20479008e7bb9977fd2d96138ad3601
Shudharshan07 <shudharshan07@gmail.com>
2025-08-03 10:43:32 +0530

    Update model.py

--- model.py
+++ model.py
@@ -87,12 +87,3 @@ app.add_handler(telegram.ext.CommandHandler('help', help))
 app.add_handler(telegram.ext.MessageHandler(telegram.ext.filters.PHOTO, image))
 
 app.run_polling(poll_interval=3)
-
-
-
-# content 3 and style 9
-# content 2 and style 15
-# content 3 and style 15
-# content 2 and style 16
-
-
